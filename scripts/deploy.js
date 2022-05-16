154721da1bff1b2a42030fac1260a07a6de99eb4
SahilPanhotra <sahilpanhotra@gmail.com>
2022-05-16 11:59:55 +0530

    first Commit

---
+++ scripts/deploy.js
@@ -0,0 +1,24 @@
+const hre = require("hardhat");
+
+async function main() {
+    /*
+      A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
+      so whitelistContract here is a factory for instances of our Whitelist contract.
+      */
+    const whitelistContract = await hre.ethers.getContractFactory("Whitelist");
+    // here we deploy the contract
+    const deployedWhitelistContract = await whitelistContract.deploy(10);
+    // 10 is the Maximum number of whitelisted addresses allowed
+
+    // Wait for it to finish deploying
+    await deployedWhitelistContract.deployed();
+
+    console.log("Whitelisting Contract Deployed to :", deployedWhitelistContract.address)
+}
+
+main().
+then(() => process.exit(0))
+.catch((error) => {
+    console.error(error);
+    process.exit(1);
+});
